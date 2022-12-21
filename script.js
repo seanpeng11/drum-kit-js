4dfb43607993361c4bbced3b9bdbd519b9515f7a
Sean Peng <33929745+seanpeng11@users.noreply.github.com>
2022-12-21 12:48:56 +0800

    Add transition effect for keys

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`)
     if (!audio) return; //stop the function from running
+    audio.currentTime = 0; //rewind to the start
     audio.play();
+    key.classList.add('playing');
 });
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; //skip if it's not a transform
+    this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+console.log(keys);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
